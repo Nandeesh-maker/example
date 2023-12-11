e0454c9115c13afb479d93051081eda4066070cb
Nandeesh-maker <nandeeshmdoggalli@gmail.com>
2023-12-11 14:48:01 +0530

    nmd

---
+++ src/main/java/com/jsp/Myntra/App.java
@@ -0,0 +1,14 @@
+package com.jsp.Myntra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.print("hi");
+    }
+}
